3d583174b9abc68e609a7228e6fbb3638e872c98
Jess <Jess@10.13.76.61>
2019-02-01 12:07:04 -0700

    test push

--- src/Task.java
+++ src/Task.java
@@ -5,23 +5,21 @@ import java.util.ArrayList;
 public class Task {
     
     int penaltyValue; //value for tree calculations
-    char name; //Task's name
+    private char name; //Task's name
+    private int number;
     
     /*Constructor gives name based on switch and assigns
      * penalty value
      */
-    public Task(int numberName, int penalty) {
-        switch (numberName) {
-            case 1: name = 'A'; break;
-            case 2: name = 'B'; break;
-            case 3: name = 'C'; break;
-            case 4: name = 'D'; break;
-            case 5: name = 'E'; break;
-            case 6: name = 'F'; break;
-            case 7: name = 'G'; break;
-            case 8: name = 'H'; break;
-        }
+    public Task(int num, char name, int penalty) {
+        this.name = name;
+        number = num;
         penaltyValue = penalty;
     }
     public char getName() {return this.getName();}
+    
+    public int getPenalty() {return penaltyValue;}
+    
+    public int getNumber() {return number;}
+    
 }
