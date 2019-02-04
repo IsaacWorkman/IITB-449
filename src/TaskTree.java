7eb822a5115d771cd8991cd5a6f07f0492180585
Richard Williams <richardwilliams@pop-os.localdomain>
2019-02-04 12:27:21 -0700

    added work to calc penalty

--- src/TaskTree.java
+++ src/TaskTree.java
@@ -6,34 +6,34 @@ import java.util.Arrays;
 public class TaskTree {
 	//List of available tasks. Note that this must be in the form of Strings, as TaskNodes are reserved for tree construction.
 	//Note that tasks and TaskNodes are separate and not interchangeable.
-	private Deck deck; 
+	private Deck deck;
 	private TaskNode bestNode; //The best pairing of machines > tasks that has been found. Only need to store reference to deepest node of the root.
-	private int lowestPenalty; 
+	private int lowestPenalty;
 	private TaskNode root = new TaskNode();
 	private ArrayList<Character> usedTasks = new ArrayList<Character>();
-	
+
 	//TODO: Add implementation to constructor method to receive massaged input data. (Aka the "kingdex")
 	public TaskTree(Deck deck) {
 		this.deck = deck;
 		findPairs(root);
 	}
-	
-	
+
+
 	public TaskNode getBestNode() {return this.bestNode;}
 	public int getPenalty() {return this.lowestPenalty;}
-	
+
 	//Returns an ArrayList containing the current best order found.
 	public ArrayList<Character> getBestOrder(){
 		ArrayList<Character> result = new ArrayList<Character>();
 		TaskNode currentNode = this.getBestNode();
-		
+
 		while (currentNode.getParent() != null) {
 			result.add(0, currentNode.getTask().getName());
 			currentNode = currentNode.getParent();
 		}
 		return result;
 	}
-	
+
 	//Finds the best set of machine:task pairs.
 	//TODO: Halt on branches exceeding the current best penalty score.
 	public void findPairs(TaskNode currentNode) {
@@ -73,9 +73,14 @@ public class TaskTree {
 			usedTasks.remove(currentNode.getTask().getName());
 		}
 	}
-	
+
 	public int calculatePenalty(TaskNode currentNode) {
-		//TODO: calculate penalty score.
-		return 100;
+		int penalty;
+		int depth = currentNode.getDepth();
+		for (int i = 0; i < depth; i++) {
+			penalty += currentNode.getTask().getPenalty();
+			currentNode = currentNode.getParent();
+		}
+		return penalty;
 	}
 }
