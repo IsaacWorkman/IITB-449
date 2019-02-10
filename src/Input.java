11939cc7a5488e9875dc3816d9d2774a48dc350d
Karen K <41897864+karenkuri@users.noreply.github.com>
2019-02-09 20:04:39 -0700

    Update Input.java
    working on the error handling 
    Everything else should work

--- src/Input.java
+++ src/Input.java
@@ -10,7 +10,7 @@ public class Input{
 	public ArrayList<char[]> forbiddenMachine = new ArrayList<char[]>() ;
 	public ArrayList<char[]> tooNear = new ArrayList<char[]>();
 	public ArrayList<char[]> tooNearPen = new ArrayList<char[]>();
-	public char[][] machinePen = new char[8][8]; // Since size is already known make double array
+	public char machinePen[][] = new char[8][8]; // Since size is already known make double array
 
 	public Input(String[] args) throws IOException{
 		BufferedReader br = null;
@@ -22,8 +22,8 @@ public class Input{
 		File outputFile = null;
 		
 		String Name = "";
-		String pmachine = "ABCDEFGH";
-		String ptasks = "12345678";
+		String ptasks = "ABCDEFGH";
+		String pmachine = "12345678";
 			
 		// If there is less than 2 files given in args then an output file for error message 	
 		if(args.length < 2){
@@ -87,18 +87,20 @@ public class Input{
 						while(!line.isEmpty()) {
 							//System.out.println(line);
 							char[] splitline = line.toCharArray();
-							char tasks  = splitline[1];
-							char machines = splitline[4];
-							int taskin = ptasks.indexOf(tasks); 
-							//System.out.println(taskin);
-							int taskout = pmachine.indexOf(machines); 
-							//System.out.println(taskout);
+							char machines  = splitline[1];
+							System.out.println(machines);
+							char tasks = splitline[4];
+							System.out.println(tasks);
+							int taskin = pmachine.indexOf(machines); 
+							System.out.println(taskin);
+							int taskout = ptasks.indexOf(tasks); 
+							System.out.println(taskout);
 							if(taskin == -1 || taskout == -1) {
 								writer.println("Error: there are no such Characters");
 								System.exit(1);
 							}
 							//System.out.print("a\n" );
-							char[] charArray = {tasks,machines};
+							char[] charArray = {machines,tasks};
 							//forcedPartial[forcedPartial.length] = charArray; 
 							//System.out.print("b\n" );
 							forcedPartial.add(charArray);
@@ -117,15 +119,15 @@ public class Input{
 						System.out.println("forbidden machine");
 						while(!line.isEmpty()) {
 							char[] splitline = line.toCharArray();
-							char tasks  = splitline[1];
-							char machines = splitline[4];
-							int taskin = ptasks.indexOf(tasks); 
-							int taskout = pmachine.indexOf(machines); 
+							char machines  = splitline[1];
+							char tasks = splitline[4];
+							int taskin = pmachine.indexOf(machines); 
+							int taskout = ptasks.indexOf(tasks); 
 							if(taskin == -1 || taskout == -1) {
 								writer.println("Error: there are no such Characters");
 								System.exit(1);
 							}
-							char[] charArray = {tasks,machines};
+							char[] charArray = {machines,tasks};
 							forbiddenMachine.add(charArray);
 							
 							line = br.readLine(); 
@@ -139,17 +141,17 @@ public class Input{
 						System.out.println("too-near tasks");
 						while(!line.isEmpty()) {
 							char[] splitline = line.toCharArray();
-							char tasks  = splitline[1];
-							//System.out.println(tasks);
-							char machines = splitline[4];
+							char machines  = splitline[1];
 							//System.out.println(machines);
-							int taskin =pmachine.indexOf(tasks); 
-							int taskout = pmachine.indexOf(machines); 
+							char tasks = splitline[4];
+							//System.out.println(machines);
+							int taskin =ptasks.indexOf(machines); 
+							int taskout = ptasks.indexOf(tasks); 
 							if(taskin == -1 || taskout == -1) {
 								writer.println("Error: there are no such Characters");
 								System.exit(1);
 							}
-							char[] charArray = {tasks,machines};
+							char[] charArray = {machines,tasks};
 							tooNear.add(charArray);
 							//System.out.println("a");
 							line = br.readLine();
@@ -162,30 +164,53 @@ public class Input{
 			
 					// Splitting each line and adding all the characters into the array at point ij. 
 					else if (line.contains("machine penalties")) {
-						//System.out.println("Machine penalties");
+						System.out.println("Machine penalties");
 						line = br.readLine();
+		
+						char[] charArrays = new char[line.length()];
+						//System.out.println(line.length());
+						line = line.replaceAll("\\s+", "");
+						charArrays = line.toCharArray();
+				
+						//System.out.println(line.length());
+						//System.out.println(charArrays[7]);
+						int count = 0;
 						while(!line.isEmpty()) {
-							//System.out.println("c);
-							String[] fix = line.split(" ");
-							for( int i = 0; i <= 7; i++) {
+							for( int i = 0; i < 1; i++) {	
 								for( int j = 0; j <= 7; j++) {
-									char n = fix[j].charAt(0);
-									machinePen[i][j] = n;
-									System.out.print(machinePen[i][j]);
+									char c = charArrays[j];
+									machinePen[j][count] = c;
+									//System.out.print(machinePen[i][j]);
+									//System.out.println(i+"!!!!");
+									//System.out.println(j);
 								}
-							} line = br.readLine();
+								count++;
+							} 
+							line = br.readLine();
+						//System.out.println(line.length());
+						line = line.replaceAll("\\s+", "");
+						charArrays = line.toCharArray();
+						//System.out.println(charArrays);
+							
+							//System.out.print(machinePen[0][7]);
 							//System.out.println(line);		
 						}
 						line = br.readLine();
-						
-						for (int k=0; k<=7; k++){
+
+						//System.out.print(machinePen[2][1]);
+						/*for (int k=0; k<=7; k++){
 								for (int h=0; h<=7; h++){
 									System.out.print(machinePen[k][h]);
 
 								}
-						}
+						}*/
+
 						//System.out.println(machinePen[0][0]);
-						//System.out.println(machinePen[1][1]);
+						System.out.println(machinePen[0][0]);
+						System.out.println(machinePen[1][1]);
+						System.out.println(machinePen[2][2]);
+						System.out.println(machinePen[3][3]);
+						System.out.println(machinePen[4][5]);
 
 					}
 					// Splitting the read line and adding 2 variables into the character array if the character is (
